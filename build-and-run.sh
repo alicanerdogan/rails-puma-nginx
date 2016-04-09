24bc33f9246dcdbbbc4c0548c83244f6a8ba4129
alicanerdogan <aerdogan07@gmail.com>
2016-04-09 18:03:25 +0300

    Updated for postgres container initialization.

--- build-and-run.sh
+++ build-and-run.sh
@@ -7,5 +7,9 @@ fi
 
 set -e
 
+mkdir /home/db-volume
+docker run --name curtain-db -e POSTGRES_PASSWORD=$1 -v /home/db-volume:/root/db-volume -d postgres
+
+mkdir /home/app-volume
 docker build --build-arg DATABASE_USER=postgres --build-arg DATABASE_PASSWORD=$1 --build-arg DATABASE_HOST=curtain-db --build-arg APP_URL=https://github.com/alicanerdogan/Curtain.git -t decauville:latest .
 docker run -d --name curtain -v /home/app-volume:/root/app-volume -p 80:80 --link curtain-db decauville:latest
